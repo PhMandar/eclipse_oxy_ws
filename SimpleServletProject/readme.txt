b202ddbe29aa47d409a14bb42d98a50247aaa0c3
PhMandar <mandar.m.phatak@gmail.com>
2017-12-07 09:54:07 +0530

    Submitting first template of servlet program with simple doGet and doPost methods in it.

---
+++ SimpleServletProject/readme.txt
@@ -0,0 +1,4 @@
+To run this program :
+1. Start the Tomcat server
+2. In the browser type following URL :
+http://localhost:9999/SimpleServletProject/SimpleForm.html
---
+++ SimpleServletProject/src/org/mandy/javabrains/XmlServlet.java
@@ -0,0 +1,22 @@
+package org.mandy.javabrains;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class XmlServlet extends HttpServlet {
+	 
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -719546934057013317L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("<h2>This is XML servlet !! </h2>");
+	}
+}
